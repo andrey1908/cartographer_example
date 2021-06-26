146e4d560def6e4b0af2dbae7969b3b3bb66ba05
andrey1908 <andrey.krishtopik@mail.ru>
2021-06-26 13:32:20 +0300

    move prepare_poses_for_evaluation.py to ros_utils repo

--- scripts/auto_evaluation_on_SDBCS_Husky.py
+++ scripts/auto_evaluation_on_SDBCS_Husky.py
@@ -10,31 +10,31 @@ bags.sort()
 for bag in bags:
     command = "roslaunch cartographer_example cartographer_offline.launch bag_filenames:=/home/cds-jetson-host/catkin_ws/{} \
         urdf_filenames:=/home/cds-jetson-host/catkin_ws/src/cartographer_example/urdf/SDBCS_Husky.urdf \
-        save_state_filename:=/home/cds-jetson-host/catkin_ws/cartographer_test/3d/no_opt/{}.pbstream".format(bag, int(bag[:2]))
+        save_state_filename:=/home/cds-jetson-host/catkin_ws/cartographer_test/3d/all_opt/{}.pbstream".format(bag, int(bag[:2]))
     process = subprocess.Popen(command.split())
     process.communicate()
 
 for bag in bags:
     command = "rosrun cartographer_ros cartographer_dev_pbstream_trajectories_to_rosbag \
-        -input /home/cds-jetson-host/catkin_ws/cartographer_test/3d/no_opt/{}.pbstream \
-        -output /home/cds-jetson-host/catkin_ws/cartographer_test/3d/no_opt/{}.bag".format(int(bag[:2]), int(bag[:2]))
+        -input /home/cds-jetson-host/catkin_ws/cartographer_test/3d/all_opt/{}.pbstream \
+        -output /home/cds-jetson-host/catkin_ws/cartographer_test/3d/all_opt/{}.bag".format(int(bag[:2]), int(bag[:2]))
     process = subprocess.Popen(command.split())
     process.communicate()
 
 for bag in bags:
-    command = "python /home/cds-jetson-host/catkin_ws/src/cartographer_example/scripts/prepare_poses_for_evaluation.py \
+    command = "python /home/cds-jetson-host/catkin_ws/src/ros_utils/scripts/prepare_poses_for_evaluation.py \
         -bag-gt /home/cds-jetson-host/catkin_ws/{} -gt-topic /atlans_odom \
-        -bag-res /home/cds-jetson-host/catkin_ws/cartographer_test/3d/no_opt/{}.bag -res-topic trajectory_0 \
-        -out-gt /home/cds-jetson-host/slam_validation/cartographer/3d/no_opt/gt/{}.txt \
-        -out-res /home/cds-jetson-host/slam_validation/cartographer/3d/no_opt/results/{}.txt".format(bag, int(bag[:2]), int(bag[:2]), int(bag[:2]))
+        -bag-res /home/cds-jetson-host/catkin_ws/cartographer_test/3d/all_opt/{}.bag -res-topic trajectory_0 \
+        -out-gt /home/cds-jetson-host/slam_validation/cartographer/3d/all_opt/gt/{}.txt \
+        -out-res /home/cds-jetson-host/slam_validation/cartographer/3d/all_opt/results/{}.txt".format(bag, int(bag[:2]), int(bag[:2]), int(bag[:2]))
     process = subprocess.Popen(command.split())
     process.communicate()
 
 for projection in ['xy', 'xz']:
     command = "python /home/cds-jetson-host/slam_validation/evaluate_poses.py \
-        --dir_gt /home/cds-jetson-host/slam_validation/cartographer/3d/no_opt/gt \
-        --dir_result /home/cds-jetson-host/slam_validation/cartographer/3d/no_opt/results \
-        --dir_output /home/cds-jetson-host/slam_validation/cartographer/3d/no_opt/output_{} \
+        --dir_gt /home/cds-jetson-host/slam_validation/cartographer/3d/all_opt/gt \
+        --dir_result /home/cds-jetson-host/slam_validation/cartographer/3d/all_opt/results \
+        --dir_output /home/cds-jetson-host/slam_validation/cartographer/3d/all_opt/output_{} \
         --gt_format kitti --result_format kitti --projection {}".format(projection, projection)
     process = subprocess.Popen(command.split())
     process.communicate()
