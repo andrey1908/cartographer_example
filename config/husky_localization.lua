1af101b53d6769d9d9553864acd1ab6921ee1d1f
andrey1908 <andrey.krishtopik@mail.ru>
2022-12-31 15:32:53 +0300

    change global_constraints_per_node from 1 -> 0.6 in husky configs

--- config/husky_localization.lua
+++ config/husky_localization.lua
@@ -88,7 +88,7 @@ POSE_GRAPH.optimize_every_n_nodes = 2
 -- Constraint search --
 POSE_GRAPH.max_local_constraint_distance = 15.
 POSE_GRAPH.local_constraints_per_node = 0.6
-POSE_GRAPH.global_constraints_per_node = 1
+POSE_GRAPH.global_constraints_per_node = 0.6
 POSE_GRAPH.constraint_builder.min_score = 0.40
 POSE_GRAPH.constraint_builder.global_localization_min_score = 0.40
 POSE_GRAPH.global_constraint_search_after_n_seconds = 0.
